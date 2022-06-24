37d986f8e910d7e9c57c269be1c7bf50ac3ea7a3
ThauCamilly0 <94691159+ThauCamilly0@users.noreply.github.com>
2022-06-24 11:35:03 -0300

    Add files via upload
    Site sobre filme e séries, feito por: Thauane Camilly Porto

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ENTRETENIMENTOS</title>
+</head>
+<body>
+    <div class="banner">
+ <img src="banner2.png" widht="150"height="190">
+ 
+ <h2>ENTRETENIMENTO<h2>
+</div>
+    <div class="home">
+    <br><br><br><br>
+        <a href= "novela.php"><img src="novela2.png"></a>
+        <br><br><br>
+        <a href= "serie.php"><img src="serie1.png"></a>
+</body>
+</html>
---
+++ novela.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet"href="series.css">
+</head>
+<body>
+<div class="cabecalho">
+ <img src="imagem4.jpg"widht="150"height="190" > 
+ </div>
+  <div text-align="center">  <h2> Cadastre suas novelas preferidas</h2> </div>
+        <hr size=6 color=black widht=200>
+        <br>
+   <form method="post" id="cadastro" action="cadastronovela.php">
+ 
+      
+           
+   <h3><label for="titulo">Título:</label><input type="text" name="titulo"id="titulo" size="30" maxlenght="100" placeholder="Digite o titulo"></h3>
+       <h3><label for="atrizprotagonista">Atríz Protagonista:</label><input type="text" name="atrizprotagonista"id="atrizprotagonista" size="30" maxlenght="100" placeholder="Digite a atriz protagonista"></h3>
+       <h3><label for="atorprotagonista">Ator Protagonista:</label><input type="text" name="atorprotagonista"id="atorprotagonista" size="30" maxlenght="100" placeholder="Digite Autor protagonista"></h3>
+       <h3><label for="totalepisodios">Total episódios:</label><input type="number" name="totalepisodios"id="totalepisodios" size="30" maxlenght="100" placeholder="Digite total de episódio"></h3>
+       <h3><label for="sinopse">Sinopse:</label><input type="text" name="sinopse"id="sinopse" size="30" maxlenght="100" placeholder="Digite a sinopse"></h3>
+<header class="botao">
+<input type="image"src="Group1.jpg" style="border:0;" height="32" width="150">
+</header>
+<div class="rodape"><img src="imagem6.jpg"widht="200"height="200"  > </div>
+</form>
+        </body>
+</html>
